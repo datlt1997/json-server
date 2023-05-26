fca0e48fa4d91598e95eaf4e0567290457b126d3
NTA LE TIEN DAT <datlt.nta@gmail.com>
2023-05-26 11:02:27 +0700

    change router

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/invite/:id/show': '/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
